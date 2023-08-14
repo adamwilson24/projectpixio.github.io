fa507f50cff76f158cacf3f850d57e015940d391
Adam Wilson <104118496+adamwilson24@users.noreply.github.com>
2023-08-14 07:29:16 -0500

    Added blank roadmap phases

--- main.js
+++ main.js
@@ -3,3 +3,11 @@ const hamburger = document.querySelector('.hamburger');
 hamburger.addEventListener('click', function() {
   this.classList.toggle('is-active');
 })
+
+const logo = document.getElementById('page-logo');
+logo.addEventListener('click', () => {
+  window.scrollTo({
+    top:0,
+    behavior:'smooth'
+  });
+});
