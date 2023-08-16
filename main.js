33f593acf0b08e3c164334086aa6feaf32cd9a65
Adam Wilson <104118496+adamwilson24@users.noreply.github.com>
2023-08-15 20:43:22 -0500

    Phase click

--- main.js
+++ main.js
@@ -11,3 +11,11 @@ logo.addEventListener('click', () => {
     behavior:'smooth'
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const clickableDiv = document.getElementById("clickableDiv");
+
+  clickableDiv.addEventListener("click", function () {
+    this.classList.add("active"); // Add a class to trigger the transition
+  });
+});
